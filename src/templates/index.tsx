944f13d7e54c9a822277f1fe006ee1c4da1921b9
Barry L <barrylim.ida@gmail.com>
2020-10-04 16:41:23 +0800

    First commit

---
+++ src/templates/index.tsx
@@ -0,0 +1,18 @@
+import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
+import { templates as customTemplate } from "./customTemplate";
+import styled from "@emotion/styled";
+import { CustomTemplate } from "./customTemplate/customTemplate";
+
+export const registry: TemplateRegistry<any> = {
+  custom: customTemplate,
+  red: [
+    {
+      id: "custom-red",
+      label: "Red Custom Template",
+      template: styled(CustomTemplate)`
+        color: red;
+      `
+    }
+  ],
+  COC: templates
+};
