254e2f4c6bd1ea54894e7f1a52976bf26d1d9c6a
Barry L <barrylim.ida@gmail.com>
2020-10-04 22:59:44 +0800

    Update with COC template

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,18 +1,6 @@
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
-import { templates as customTemplate } from "./customTemplate";
-import styled from "@emotion/styled";
-import { CustomTemplate } from "./customTemplate/customTemplate";
+import { templates } from "./coc";
 
 export const registry: TemplateRegistry<any> = {
-  custom: customTemplate,
-  red: [
-    {
-      id: "custom-red",
-      label: "Red Custom Template",
-      template: styled(CustomTemplate)`
-        color: red;
-      `
-    }
-  ],
   COC: templates
 };
